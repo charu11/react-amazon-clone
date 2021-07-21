38b2b0107c3ab77208a6bed74621ef80302ae5c9
charun umesh <charunumesh@gmail.com>
2021-07-21 11:22:27 +0530

    payment page added

---
+++ src/Payment.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  const history = useHistory();
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+
+          <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map(item => (
+                <CheckoutProduct 
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}/>
+            ))}
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
